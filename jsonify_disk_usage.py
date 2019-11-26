4e6468cf53fdcc09beacc5ac8cb1fc51bfcf2a04
David Riser <dmriser@jlab.org>
2019-11-26 14:20:14 -0500

    sorted du

--- jsonify_disk_usage.py
+++ jsonify_disk_usage.py
@@ -6,7 +6,7 @@ Disk usage utility.
 
 import argparse
 import json
-
+from collections import OrderedDict 
 
 def tokenize(line):
     return line.strip().split()
@@ -23,6 +23,22 @@ def line_is_accepted(line):
 
 def is_subdir(folder_name):
     return (len(folder_name.split('/')) == 3)
+
+def create_ordered_dict(data, sort_key='total_size'):
+
+    def giga_stripper(line):
+        return int(line[:-1])
+
+    output = OrderedDict()    
+    users = data.keys() 
+    sizes = [giga_stripper(d[sort_key]) for d in data.values()]
+    
+    for size, user in sorted(zip(sizes, users))[::-1]:
+        output[user] = data[user]
+        output[user]['sub_directories'] = sorted(data[user]['sub_directories'], 
+                                                 key=lambda x: giga_stripper(x['size']))[::-1]
+
+    return output 
 
 if __name__ == '__main__':
 
@@ -52,6 +68,6 @@ if __name__ == '__main__':
             )
         else:
             json_dict[user]['total_size'] = size
-            
+
     with open(args.output, 'w') as json_file:
-        json.dump(json_dict, json_file, indent=4)
+        json.dump(create_ordered_dict(json_dict), json_file, indent=4)
