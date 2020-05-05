901964409f4310218bb3bbccd542f25b02dfd271
maureeungaro <maureeungaro@gmail.com>
2020-05-05 09:00:50 -0400

    more readable

--- testRelease.sh
+++ testRelease.sh
@@ -2,9 +2,10 @@
 
 set grepo = https://github.com/mit-mc-clas12
 
-set destination=/group/clas/www/gemc/html/test
 
 ### SubMit/web_interface on the gemc server
+set destination=/group/clas/www/gemc/html/test
+
 set installDir=$destination
 rm -rf $installDir ; mkdir -p  $installDir ;  cd $installDir
 foreach repo (web_interface)
@@ -39,6 +40,7 @@ echo
 
 ### SubMit on /group
 set destination=/group/clas12/SubMit/test
+
 set installDir=$destination/SubMit
 rm -rf $installDir ; mkdir -p  $installDir ;  cd $installDir
 foreach repo (utils server client)
@@ -54,12 +56,12 @@ foreach repo (utils server client)
 end
 echo
 
-# installing indexMaintanance.php to index.php
+# copy indexMaintanance.php to index.php
 echo
-echo Maintainance mode: mv indexMaintanance.php index.php
+echo Maintainance mode: cp indexMaintanance.php index.php
 echo
 cd /u/group/clas/www/gemc/html/web_interface
-mv indexMaintanance.php index.php
+cp indexMaintanance.php index.php
 echo done
 echo
 
