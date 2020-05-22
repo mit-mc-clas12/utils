2157d53186c2b4e5113d0eee6a0a58f4dff0e5a4
ma <maurizio.ungaro@cnu.edu>
2020-05-22 14:33:46 -0400

    grepping owner, fixing crontab comment

--- shScripts/osgQuery.sh
+++ shScripts/osgQuery.sh
@@ -1,11 +1,11 @@
 #!/bin/csh -f
 
 # Crontab command for every 1 minute 
-# */10 * * * * ~/osgStats.sh 
+# */2 * * * * ~/osgQuery.sh
 
 ### going to web interface stats_results
 cd /group/clas/www/gemc/html/web_interface/stats_results
 rm gemcRunning.log osgLog.json
 
-condor_q -submitter gemc > gemcRunning.log
+condor_q -submitter gemc | grep OWNER -A100 > gemcRunning.log
 python /group/clas12/SubMit/utils/jsonify_logfile.py --logfile=gemcRunning.log --output=osgLog.json
