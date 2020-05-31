aa3278aae3b6bbe0145825a983258d37685a7036
robertej19 <robertej@mit.edu>
2020-05-31 11:15:42 -0400

    fixed typo

--- get_condor_q.py
+++ get_condor_q.py
@@ -32,7 +32,7 @@ def get_htcondor_q():
                     else:
                         print("anomylous job status of {0}, investigate more".format(job_status))
                 else:
-                    utils.printer("found new batch: {0}".format(batch_id),1)
+                    utils.printer("found new batch: {0}".format(batch_id))
                     total_jobs_for_batch = job.get("TotalSubmitProcs") #Get total number of jobs in batch submitted
                     start_date_unix = job.get("QDate")  #Get submitted date
 
