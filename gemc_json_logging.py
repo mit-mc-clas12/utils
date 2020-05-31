c282c32bcddb212329dde10754aa11d84f7bc5f7
robertej19 <robertej@mit.edu>
2020-05-31 11:12:59 -0400

    turned off print

--- gemc_json_logging.py
+++ gemc_json_logging.py
@@ -79,7 +79,6 @@ def create_json_dict(args):
 
             sql.execute("SELECT COUNT(pool_node) FROM submissions WHERE pool_node = {}".format(osg_id))
             count = sql.fetchall()[0][0]
-            print("count is {0}".format(count))
 
             #I dont get exactly what is going on here. How can we have a zero in the DB but nonzero  in condor?
             #looking at this more, it is only 1 if exists in db, or 0 if not in db
