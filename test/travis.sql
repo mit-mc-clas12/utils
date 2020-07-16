b516e9028d13fa930cdea7d9b43ef5aa0ae54bcc
robertej19 <robertej@mit.edu>
2020-07-16 01:05:54 -0400

    still enableing mysql on travis:

--- test/travis.sql
+++ test/travis.sql
@@ -1,6 +1,5 @@
 # Create Testuser
 CREATE USER 'dev'@'localhost';
-SET PASSWORD FOR 'dev'@'localhost' = 'devpassword';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
 # Create DB
 CREATE DATABASE IF NOT EXISTS `CLAS12OCR`;
