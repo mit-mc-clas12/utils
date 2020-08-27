b01edf616078f4390d0c99fc8a8f8d6b1decb44c
ma <maurizio.ungaro@cnu.edu>
2020-08-27 10:01:28 -0400

    comments for runTest

--- scardTests/runTests.sh
+++ scardTests/runTests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# cd /group/clas12/SubMit/test/SubMit/utils/scardTests
+# ./runTests.sh
+
 gcards="rga_fall2018_bgmerging.txt rga_spring2018_nobg.txt rgb_spring2019_nobg.txt rgk_fall2018_FTOn_nobg.txt \
         rga_fall2018_nobg.txt      rga_spring2019_nobg.txt rgk_fall2018_FTOff_nobg.txt"
 
@@ -18,3 +21,15 @@ echo Submitting Jobs to OSG
 cd /group/clas12/SubMit/test/SubMit/server/
 python src/Submit_UserSubmission.py -s -t --test_database
 echo
+
+# Output:
+# job_/output/simu_0/   rga_fall2018_bgmerging
+# job_/output/simu_0/   rga_spring2018_nobg
+# job_/output/simu_0/   rgb_spring2019_nobg
+# job_/output/simu_0/   rgk_fall2018_FTOn_nobg
+# job_/output/simu_0/   rga_fall2018_nobg
+# job_/output/simu_0/   rga_spring2019_nobg
+# job_/output/simu_0/   rgk_fall2018_FTOff_nobg
+
+
+
