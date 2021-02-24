3eee1ffc357b504219383826c478af1a49bdf6e8
maureeungaro <maureeungaro@gmail.com>
2021-02-24 10:52:21 -0500

    script to check cvmfs load

---
+++ scripts/checkOasis.sh
@@ -0,0 +1,13 @@
+#!/bin/csh -f
+
+rm -f ~/oasisMissing.txt
+grep "Transport endpoint is not connected" log/*.err | awk -F. '{print $3}' | sort -u > ~/oasisMissing.txt
+
+echo
+echo Jobs with missing oasis:
+cat ~/oasisMissing.txt
+echo
+
+foreach j ( `cat ~/oasisMissing.txt` )
+	grep JOB_GLIDEIN_SiteWMS_Slot log/job*.$j".log" | tail -1
+end
