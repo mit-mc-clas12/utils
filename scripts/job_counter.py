cb66166f74a2afb2add2dd5470c876db2d1e26e5
maureeungaro <maureeungaro@gmail.com>
2021-10-26 14:49:38 -0400

    updated shouldBeSubmitted

--- scripts/job_counter.py
+++ scripts/job_counter.py
@@ -1,13 +1,12 @@
 import json
 
-
 def shouldBeSubmitted(user_name, json_file="/home/gemc/logs/osgLog.json", idle_limit=1000000):
     # Open JSON file and load as dictionary
     f = open(json_file,)
     data = json.load(f)
     f.close()
 
-    #Convert json data into useable dictionary
+    # Convert json data into useable dictionary
     user_info = {}
     columns = ['total','done','run','idle','hold']
 
@@ -15,23 +14,26 @@ def shouldBeSubmitted(user_name, json_file="/home/gemc/logs/osgLog.json", idle_l
         if not job['user'] in user_info.keys():
             user_info[job['user']] = {}
             for c in columns:
-                user_info[job['user']][c] = int(job[c])
+                if job[c] != 'No data':
+                    user_info[job['user']][c] = int(job[c])
+                else:
+                    user_info[job['user']][c] = 0
         else:
             for c in columns:
                 user_info[job['user']][c] += int(job[c])
 
     # Perform logic
-    user = user_info[user_name]
-    if (user['idle']+user['run'])>idle_limit:
-        return False
+    if user_name in user_info:
+        user = user_info[user_name]
+        if (user['idle']+user['run'])>idle_limit:
+            return False
+        else:
+            return True
     else:
         return True
-
 
 if __name__ == "__main__":
     idle_limit = 10000  # Set the threshold of idle jobs
     user_name = "robertej"
-    bool_over_limit = shouldBeSubmitted(user_name,"example.json",idle_limit)
-    print("{} over {}: {}".format(user_name, idle_limit, bool_over_limit))
-
-
+    bool_submit = shouldBeSubmitted(user_name,"/home/gemc/logs/osgLog.json",idle_limit)
+    print("{} over {}. Should submit: {}".format(user_name, idle_limit, bool_submit))
