7efd3982435fd1edb107a5b84eb6a9d48367937a
maureeungaro <maureeungaro@gmail.com>
2021-10-27 10:18:02 -0400

    scopying osglog

--- scripts/osgQuery.sh
+++ scripts/osgQuery.sh
@@ -3,3 +3,5 @@
 # creating osgLog.json inside /home/gemc/logs
 cd /home/gemc/logs
 python /home/gemc/software/Submit/utils/gemc_json_logging.py
+scp osgLog.json dtn1902-ib:/lustre19/expphy/volatile/clas12/osg2/
+
