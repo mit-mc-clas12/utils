710085bfc6c10abf448dca4450496eb955145b3d
Maurizio Ungaro <ungaro@jlab.org>
2023-04-03 09:58:27 -0400

    crontatb on both production and tests DB

--- scripts/gemcSubmitCron.sh
+++ scripts/gemcSubmitCron.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 scriptDir=/home/gemc/software/Submit/server/src
+scriptDirT=/home/gemc/software/Submit/test/server/src
 
 python3 $scriptDir/Submit_UserSubmission.py -s
+python3 $scriptDirT/Submit_UserSubmission.py -s -t --test_database
