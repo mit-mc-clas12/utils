3dd8733ef69190acc18c9e41927d06c96dae96ee
Maurizio Ungaro <maureeungaro@gmail.com>
2025-01-13 14:52:46 -0500

    copying files to /volatile/clas12/osg instead of osg2

--- scripts/osgQuery.sh
+++ scripts/osgQuery.sh
@@ -3,4 +3,4 @@
 # creating osgLog.json inside /home/gemc/logs
 cd /home/gemc/logs
 python3 /home/gemc/software/Submit/utils/gemc_json_logging.py
-scp osgLog.json dtn1902:/lustre/expphy/volatile/clas12/osg2/
+scp osgLog.json dtn1902:/lustre/expphy/volatile/clas12/osg/
