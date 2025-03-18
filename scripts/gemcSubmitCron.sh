599cdb7a6ac9434e6461bf372ed9fc6593bfc949
Maurizio Ungaro <maureeungaro@gmail.com>
2025-03-18 09:00:08 -0400

    do not submit if we have more than 80K jobs

--- scripts/gemcSubmitCron.sh
+++ scripts/gemcSubmitCron.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+# Run the command and extract the total for query using grep and awk
+total=$(condor_q -totals | grep "Total for query:" | awk '{print $4}')
+
+# Check if the extracted number is greater than 80,000
+if [ "$total" -gt 80000 ]; then
+    echo "Total jobs in queue is greater than 80,000"
+    echo "Skipping submission"
+    exit 0
+else
+    echo "Total jobs in queue: $total"
+    echo "Submitting 1 job..."
+fi
+
 scriptDir=/home/gemc/software/Submit/server/src
 scriptDirT=/home/gemc/software/Submit/test/server/src
 
