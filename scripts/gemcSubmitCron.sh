d291244b963abd661cc2767edddc1ae21e356973
Maurizio Ungaro <maureeungaro@gmail.com>
2025-10-06 11:14:25 -0400

    test come first

--- scripts/gemcSubmitCron.sh
+++ scripts/gemcSubmitCron.sh
@@ -14,8 +14,8 @@ else
     echo "Submitting 1 job..."
 fi
 
-scriptDir=/home/gemc/software/Submit/server/src
 scriptDirT=/home/gemc/software/Submit/test/server/src
+scriptDir=/home/gemc/software/Submit/server/src
 
-python3 $scriptDir/Submit_UserSubmission.py -s
 python3 $scriptDirT/Submit_UserSubmission.py -s -t --test_database
+python3 $scriptDir/Submit_UserSubmission.py -s
