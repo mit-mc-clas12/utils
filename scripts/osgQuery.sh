cb22c07c4e07279304045a00b80bb502e75bf80e
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-31 14:46:08 -0400

    added logs to pelican put

--- scripts/osgQuery.sh
+++ scripts/osgQuery.sh
@@ -4,4 +4,6 @@
 cd /home/gemc/logs
 setenv PYTHONPATH /home/gemc/software/Submit
 python3 -m utils.gemc_json_logging
-pelican object put osgLog.json osdf:///jlab-osdf/clas12/volatile/osg/osgLog.json
+pelican object put osgLog.json         osdf:///jlab-osdf/clas12/volatile/osg/osgLog.json
+pelican object put osg-devel.json      osdf:///jlab-osdf/clas12/volatile/osg/osg-devel.json
+pelican object put osg-production.json osdf:///jlab-osdf/clas12/volatile/osg/osg-production.json
